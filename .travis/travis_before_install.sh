ad0b8feb267c0e26743261995c58ce5c9a25404f
Philipp Storz <philipp.storz@bareos.com>
2018-05-28 13:03:43 +0200

    fixup

--- .travis/travis_before_install.sh
+++ .travis/travis_before_install.sh
@@ -1,12 +1,46 @@
 #!/bin/bash
 
-#sudo apt-get -qq update
-#cd core
-#dpkg-checkbuilddeps 2> /tmp/dpkg-builddeps || true
-#cat /tmp/dpkg-builddeps
-#sed -e "s/^.*:.*:\s//" -e "s/\s([^)]*)//g" /tmp/dpkg-builddeps > /tmp/build_depends
-#echo "additional packages required for building:"; cat /tmp/build_depends
-#sudo xargs --arg-file /tmp/build_depends apt-get -q --assume-yes install fakeroot
-#dpkg -l
-#true
+print_header()
+{
+   TEXT="$1"
+   printf "#\n"
+   printf "# %s\n" "$TEXT"
+   printf "#\n"
+}
 
+if [ "${COVERITY_SCAN}" ]; then
+   cd core
+   # run configure with default options
+   debian/rules override_dh_auto_configure
+   eval "$COVERITY_SCAN_BUILD"
+else
+   cd regress
+   echo '
+MAKEOPT=-j4
+BAREOS_SOURCE="`pwd`/../core"
+EMAIL=my-name@domain.com
+SMTP_HOST="localhost"
+DBTYPE="postgresql"
+WHICHDB=-D${DBTYPE}=yes
+TAPE_DRIVE="/dev/null"
+AUTOCHANGER="/dev/null"
+DRIVE1=0
+DRIVE2="none"
+SLOT1=1
+SLOT2=2
+TAPE_DRIVE1="/dev/null"
+AUTOCHANGER_SCRIPT=mtx-changer
+db_name="regress"
+db_user="regress"
+db_password=""
+TCPWRAPPERS=-Dtcp-wrappers=yes
+OPENSSL=-Dopenssl=yes
+HOST="127.0.0.1"
+SCSICRYPTO="-Dscsi-crypto=yes"
+BASEPORT=8101
+SITE_NAME=travis-bareos-${HOST}
+DEVELOPER=-Ddeveloper=yes
+COVERAGE=-Dcoverage=yes
+' > config
+
+fi

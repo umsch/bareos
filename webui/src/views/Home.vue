223b8a4ac6c10911e566581910e26fb1084b75c8
Torsten Ueberschar <tu@uesome.de>
2018-10-01 17:43:37 +0200

    the rest

---
+++ webui/src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">
+    <HelloWorld/>
+    <Jobs/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+import Jobs from '@/components/Jobs.vue'
+
+export default {
+  name: 'home',
+  components: {
+    HelloWorld,
+    Jobs
+  }
+}
+</script>

7bbd2eee43f55bb339bacea1b708cfaf6bdbcb57
Torsten Ueberschar <torsten@ueberschar.de>
2024-08-09 13:48:33 +0200

    scaffold vue and grpc stubs genration

---
+++ web-restore/bareos-ui/src/components/HelloGrpc.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>{{ message }}</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+import {ref, onMounted} from 'vue';
+import {RestoreClient} from '@/generated/restore.client';
+import {GrpcWebFetchTransport} from '@protobuf-ts/grpcweb-transport'
+
+const message = ref('');
+
+const transport = ref(new GrpcWebFetchTransport({baseUrl: 'http://127.0.0.1:9090'}));
+
+onMounted(async() => {
+  const client = new RestoreClient(transport.value);
+  const response = await client.listSessions({})
+
+  console.log("hier: ", response);
+     message.value = response.response;
+
+});
+</script>
---
+++ web-restore/bareos-ui/src/views/AboutView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="about">
+    <HelloGrpc />
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
+<script setup lang="ts">
+import HelloGrpc from "@/components/HelloGrpc.vue";
+</script>

df4fcd3b8d684ea8d8acbcd2f017ab9cd0224d2f
Torsten Ueberschar <torsten@ueberschar.de>
2024-08-10 13:22:12 +0200

    activate Oruga with theme-bulma

--- web-restore/bareos-ui/src/main.ts
+++ web-restore/bareos-ui/src/main.ts
@@ -1,14 +1,16 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
 import App from './App.vue'
 import router from './router'
 
-const app = createApp(App)
+import Oruga from '@oruga-ui/oruga-next'
 
-app.use(createPinia())
-app.use(router)
+import { bulmaConfig } from '@oruga-ui/theme-bulma'
+import '@oruga-ui/theme-bulma/dist/bulma.css'
+
+const app = createApp(App)
+  .use(Oruga, bulmaConfig)
+  .use(createPinia())
+  .use(router)
 
 app.mount('#app')

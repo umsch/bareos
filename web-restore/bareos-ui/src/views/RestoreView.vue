bdb453c5ff3fc84bbcc99f3a785de6f7ed6ad8dc
Torsten Ueberschar <torsten@ueberschar.de>
2024-08-10 16:51:37 +0200

    add RestoreView

---
+++ web-restore/bareos-ui/src/views/RestoreView.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+
+import { ref } from 'vue'
+import { OIcon } from '@oruga-ui/oruga-next'
+
+const activeStep = ref('1')
+const prevIcon = ref('chevron-left')
+const nextIcon = ref('chevron-right')
+
+</script>
+
+<template>
+  <section>
+    <o-steps
+      variant="primary"
+      :v-model="activeStep"
+      animated
+      rounded
+      has-navigation
+      :icon-prev="prevIcon"
+      :icon-next="nextIcon"
+    >
+      <o-step-item
+        value="1"
+        step="1"
+        label="eins"
+        clickable>
+        <h1>Account</h1>
+        Lorem ipsum dolor sit amet.
+      </o-step-item>
+    </o-steps>
+  </section>
+
+
+</template>
+
+<style scoped>
+
+</style>

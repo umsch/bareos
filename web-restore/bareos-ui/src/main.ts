4bb8903d86cfa6218311fabc0d2437f53d657845
Torsten Ueberschar <torsten@ueberschar.de>
2024-08-10 16:50:18 +0200

    enabled fontawesome and bulma

--- web-restore/bareos-ui/src/main.ts
+++ web-restore/bareos-ui/src/main.ts
@@ -8,9 +8,24 @@ import Oruga from '@oruga-ui/oruga-next'
 import { bulmaConfig } from '@oruga-ui/theme-bulma'
 import '@oruga-ui/theme-bulma/dist/bulma.css'
 
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+library.add(fas);
+
+
+const config = {
+  ...bulmaConfig,
+  iconComponent: "vue-fontawesome",
+  iconPack: "fas",
+};
+
+
+
 const app = createApp(App)
-  .use(Oruga, bulmaConfig)
+  .use(Oruga, config)
   .use(createPinia())
   .use(router)
+  .component("vue-fontawesome", FontAwesomeIcon);
 
 app.mount('#app')

d14d87163021e0d6487d098f8010e65bcae72edd
Torsten Ueberschar <torsten@ueberschar.de>
2024-08-15 13:52:12 +0200

    Fix Menu

--- web-restore/bareos-ui/src/App.vue
+++ web-restore/bareos-ui/src/App.vue
@@ -1,29 +1,39 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
+import { ref } from 'vue'
+
+const isMenuOpen = ref(false)
+
+const toggleMenu = () => {
+  isMenuOpen.value = !isMenuOpen.value
+}
+
 </script>
 
 <template>
   <header>
-    <div class="wrapper">
       <nav class="navbar">
-        <div class="container">
-          <div class="navbar-brand"></div>
-          <div class="navbar-menu">
-            <div class="navbar-start">
-              <a class="navbar-item">
-                <RouterLink to="/">Home</RouterLink>
-              </a>
-              <a class="navbar-item">
-                <RouterLink to="/restore">Restore</RouterLink>
-              </a>
-              <a class="navbar-item">
-                <RouterLink to="/about">About</RouterLink>
-              </a>
-            </div>
+        <div class="navbar-brand">
+          <a role="button" class="navbar-burger" :class="{'is-active': isMenuOpen}" aria-label="menu" aria-expanded="false" @click="toggleMenu">
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+          </a>
+        </div>
+        <div class="navbar-menu" :class="{'is-active': isMenuOpen}">
+          <div class="navbar-start">
+            <a class="navbar-item">
+              <RouterLink to="/">Home</RouterLink>
+            </a>
+            <a class="navbar-item">
+              <RouterLink to="/restore">Restore</RouterLink>
+            </a>
+            <a class="navbar-item">
+              <RouterLink to="/about">About</RouterLink>
+            </a>
           </div>
         </div>
       </nav>
-    </div>
   </header>
   <RouterView />
 </template>

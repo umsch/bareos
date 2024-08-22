a57e748659efe9d5af3be6518a9bc63f2bd5b6ec
Torsten Ueberschar <torsten@ueberschar.de>
2024-08-22 12:57:14 +0200

    run director in foreground

--- grpc_build.sh
+++ grpc_build.sh
@@ -4,12 +4,31 @@ cmake -DUSE_SYSTEM_FMT=ON -S /source -B /build -G Ninja
 cmake --build /build
 
 cd /build/systemtests/tests/grpc
-rm test-cleanup
+
+
+#runtime.sh
+#bin/bareos start
+#PID=$(pidof bareos_dir-grpc)
+#kill -9 $PID
+#
+
+
+
+
+## verhindern, dass testergebnisse gelöscht werden
+# rm test-cleanup
 
 runtime.sh
-ctest -R grpc
+./test-setup
+./testrunner-create-backup
+./testrunner-restore
+
+# ctest -R grpc
 
 PID=$(pidof bareos_dir-grpc)
+kill -9 $PID
+
+sbin/bareos_dir-grpc -f -d 1000 -c etc/bareos/
 
 while kill -0 $PID 2> /dev/null; do
   sleep 1

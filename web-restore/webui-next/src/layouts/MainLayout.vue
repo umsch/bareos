f7c17b1c02cb7678ce2436131535f4c9b5ad2300
Torsten Ueberschar <torsten@ueberschar.de>
2024-08-28 10:32:16 +0200

    start here

---
+++ web-restore/webui-next/src/layouts/MainLayout.vue
@@ -0,0 +1,68 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="toggleLeftDrawer"
+        />
+
+        <q-toolbar-title> Quasar App </q-toolbar-title>
+
+        <div>Quasar v{{ $q.version }}</div>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
+      <q-list>
+        <q-item-label header> Essential Links </q-item-label>
+
+        <EssentialLink
+          v-for="link in linksList"
+          :key="link.title"
+          v-bind="link"
+        />
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import EssentialLink, {
+  EssentialLinkProps,
+} from 'components/EssentialLink.vue';
+
+defineOptions({
+  name: 'MainLayout',
+});
+
+const linksList: EssentialLinkProps[] = [
+  {
+    title: 'Start',
+    caption: '',
+    icon: 'house',
+    link: '/',
+  },
+  {
+    title: 'Restore',
+    caption: 'restore from job to client',
+    icon: 'restore',
+    link: '/restore',
+  },
+];
+
+const leftDrawerOpen = ref(false);
+
+function toggleLeftDrawer() {
+  leftDrawerOpen.value = !leftDrawerOpen.value;
+}
+</script>
---
+++ web-restore/webui-next/src/router/routes.ts
@@ -0,0 +1,23 @@
+import { RouteRecordRaw } from 'vue-router';
+
+const routes: RouteRecordRaw[] = [
+  {
+    path: '/',
+    name: 'start',
+    component: () => import('pages/IndexPage.vue'),
+  },
+  {
+    path: '/restore',
+    name: 'restore',
+    component: () => import('pages/RestorePage.vue'),
+  },
+
+  // Always leave this as last one,
+  // but you can also remove it
+  {
+    path: '/:catchAll(.*)*',
+    component: () => import('pages/ErrorNotFound.vue'),
+  },
+];
+
+export default routes;

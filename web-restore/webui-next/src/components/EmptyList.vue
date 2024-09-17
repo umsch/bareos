f9c37b6806774abc3e9ee2db1cb846b4d8a9bc09
Torsten Ueberschar <torsten@ueberschar.de>
2024-09-17 08:12:25 +0200

    fix localisation

--- web-restore/webui-next/src/components/EmptyList.vue
+++ web-restore/webui-next/src/components/EmptyList.vue
@@ -1,9 +1,13 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useI18n } from 'vue-i18n'
+
+const { t } = useI18n()
+</script>
 
 <template>
   <q-item>
     <q-item-section class="text-grey">
-      {{ $t('select_empty_list') }}
+      {{ t('select_empty_list') }}
     </q-item-section>
   </q-item>
 </template>
--- web-restore/webui-next/src/components/EssentialLink.vue
+++ web-restore/webui-next/src/components/EssentialLink.vue
@@ -5,16 +5,20 @@
     </q-item-section>
 
     <q-item-section>
-      <q-item-label>{{ $t(title) }}</q-item-label>
-      <q-item-label v-if="caption" caption>{{ $t(caption) }}</q-item-label>
+      <q-item-label>{{ t(title) }}</q-item-label>
+      <q-item-label v-if="caption" caption>{{ t(caption) }}</q-item-label>
     </q-item-section>
   </q-item>
 </template>
 
 <script setup lang="ts">
+import { useI18n } from 'vue-i18n'
+
 defineOptions({
   name: 'EssentialLink',
 })
+
+const { t } = useI18n()
 
 export interface EssentialLinkProps {
   title: string
